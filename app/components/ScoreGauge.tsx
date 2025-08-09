7e48f1ca6e0d455f1d2e68489b5b20cbf4a3c4ba
JeetDas5 <jeet15083011@gmail.com>
2025-08-09 17:19:39 +0530

    feat: develop score gauge and summary

---
+++ app/components/ScoreGauge.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScoreGauge = () => {
+    return (
+        <div>
+            ScoreGauge
+        </div>
+    );
+};
+
+export default ScoreGauge;
--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import ScoreGauge from "~/components/ScoreGauge";
 
-const Summary = () => {
-    return (
-        <div>
-            Summary
-        </div>
-    );
+const Summary = ({feedback}: { feedback: Feedback }) => {
+    return (<div className="bg-white rounded-2xl w-full">
+            <div className="flex flex-row items-center p-4 gap-"></div>
+            <ScoreGauge score={feedback.overallScore || 0}/>
+        </div>);
 };
 
 export default Summary;
